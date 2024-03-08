133f758157ec2c83187149368c1e7b5eccaecc3f
Cédric Verdi <c.verdi212@gmail.com>
2024-03-08 09:44:36 -0500

    Ajout répertoire des capteurs
    Encapsulation des différents capteurs situés dans le rotor sous forme de module.

---
+++ code_pfe/ChinookSensor_PFE2024/Sensors/Src/rpm.c
@@ -0,0 +1,44 @@
+/**
+ ******************************************************************************
+ * File Name          : rpm.c
+ * Description        :
+ * Created            : Mar 8, 2024
+ * Author             : Malek Benmostefa et Cedric Verdi
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+
+/* Defines -------------------------------------------------------------------*/
+
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* Public functions  ---------------------------------------------------------*/
+
+/**
+ * @brief  Fonction qui fait quelque chose
+ * @param  None
+ * @retval None
+ */
+void fonction1(void)
+{
+    
+}
+
+/**
+ * @brief  Fonction qui fait autre chose
+ * @param  None
+ * @retval None
+ */
+void fonction2(void)
+{
+    
+}
+
+/* Private functions ---------------------------------------------------------*/
